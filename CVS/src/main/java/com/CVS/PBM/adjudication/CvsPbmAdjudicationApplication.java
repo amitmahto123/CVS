22c6501ace2e21b7f8d157b2c9e5696e4b7127a5
2414511 <2414511@LTIN545653.cts.com>
2025-09-09 10:33:03 +0530

    CVS 1st push

---
+++ CVS/src/main/java/com/CVS/PBM/adjudication/CvsPbmAdjudicationApplication.java
@@ -0,0 +1,13 @@
+package com.CVS.PBM.adjudication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CvsPbmAdjudicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CvsPbmAdjudicationApplication.class, args);
+	}
+
+}
---
+++ CVS/src/test/java/com/CVS/PBM/adjudication/CvsPbmAdjudicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CVS.PBM.adjudication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CvsPbmAdjudicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
